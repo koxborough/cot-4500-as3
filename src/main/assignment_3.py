8a02b359fa66da71aa0d9bb20e5298f4c352182b
koxborough <oxboroughkhalil@gmail.com>
2023-03-26 12:54:15 -0400

    Complete Task Two

--- src/main/assignment_3.py
+++ src/main/assignment_3.py
@@ -1,4 +1,4 @@
-def function(y, t):
+def function(t, y):
     return t - (y ** 2)
 
 def eulers_method(initial_condition, endpoint_a, endpoint_b, iterations):
@@ -6,7 +6,22 @@ def eulers_method(initial_condition, endpoint_a, endpoint_b, iterations):
     t, w = endpoint_a, initial_condition
 
     for i in range(iterations):
-        w = w + (step * (function(w, t)))
+        w = w + (step * (function(t, w)))
+        t += step
+
+    return w
+
+def runge_cutta_method(initial_condition, endpoint_a, endpoint_b, iterations):
+    step = (endpoint_b - endpoint_a) / iterations
+    t, w = endpoint_a, initial_condition
+
+    for i in range(iterations):
+        order_1 = step * function(t, w)
+        order_2 = step * function(t + (step / 2), w + (order_1 / 2))
+        order_3 = step * function(t + (step / 2), w + (order_2 / 2))
+        order_4 = step * function(t + step, w + order_3)
+
+        w = w + ((order_1 + (2 * order_2) + (2 * order_3) + order_4) / 6)
         t += step
 
     return w
@@ -18,3 +33,7 @@ if __name__ == "__main__":
     endpoint_a, endpoint_b = 0, 2
     iterations = 10
     print(eulers_method(initial_condition, endpoint_a, endpoint_b, iterations))
+    print()
+
+    # Task Two: use Runge-Kutta Method (using input from Task One)
+    print(runge_cutta_method(initial_condition, endpoint_a, endpoint_b, iterations))
